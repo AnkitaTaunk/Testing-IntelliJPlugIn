b3a2077e5bec4d3f56c3407db0473b1999d1a96f
GitHub Action <action@github.com>
2021-05-24 08:03:48 +0000

    Template cleanup

--- src/main/kotlin/com/github/ankitataunk/testingintellijplugin/MyBundle.kt
+++ src/main/kotlin/com/github/ankitataunk/testingintellijplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.ankitataunk.testingintellijplugin
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/ankitataunk/testingintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.ankitataunk.testingintellijplugin.services
+
+import com.github.ankitataunk.testingintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
